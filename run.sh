c8ba27d5c3000dd497259ecbbddfca02e4c33638
Johan Viklund <johan.viklund@nbis.se>
2018-04-16 14:44:18 +0200

    Better script for running the react app through docker

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-cd /code/react-tutorial
-yarn start
+if [ "$#" -eq 0 ] || [ ! -d "$1" ]; then
+    echo -e "Usage:\n $0 name-of-app"
+    exit 1
+fi
+
+docker run -p 3000:3000 -v "$(pwd)/$1":/code --rm -ti react-tutorial yarn start
