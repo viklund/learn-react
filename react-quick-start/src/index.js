496f34eae08895f27728ae410f7f2beefbdd4ed9
Johan Viklund <johan.viklund@nbis.se>
2018-04-16 14:58:19 +0200

    Removed default boilerplate

--- react-quick-start/src/index.js
+++ react-quick-start/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('root')
+);
+
 registerServiceWorker();
